d9a6a60c39ee38f552202730967246852461b8be
dichev <g.dichev@gmail.com>
2018-06-27 16:06:06 +0300

    Adapt cloudflare scripts

---
+++ cloudflare/check.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+'use strict';
+
+/**
+ * Usage:
+ * $ node cloudflare/check --zone dev
+ */
+
+const Deployer = require('deployer2')
+const CloudFlare = require('deployer2').plugins.CloudFlare
+const secret = require('./.secret') // TODO: temporary stored here
+
+let deployer = new Deployer()
+
+deployer
+    .option('-z, --zone <dev|gib|iom|pokerstars|asia>', 'Alias name of the cloudflare zone')
+
+    .run(async () => {
+    
+        const cfg = secret[deployer.params.zone]
+        
+        let cf = new CloudFlare(cfg.zone, cfg.email, cfg.key)
+        
+        await cf.get('custom_pages/ratelimit_block')
+        await cf.get('custom_pages/ip_block')
+        await cf.get('custom_pages/500_errors')
+        await cf.get('custom_pages/1000_errors')
+        await cf.get('custom_pages/always_online')
+        
+        await cf.get('settings/ipv6')
+        await cf.get('settings/security_level')
+        
+        await cf.get('pagerules')
+    
+})
---
+++ cloudflare/unify.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+'use strict';
+
+/**
+ * Usage:
+ * $ node cloudflare/unify --zone dev
+ */
+
+const Deployer = require('deployer2')
+const CloudFlare = require('deployer2').plugins.CloudFlare
+const secret = require('./.secret') // TODO: temporary stored here
+
+let deployer = new Deployer()
+
+deployer
+    .option('-z, --zone <dev|gib|iom|pokerstars|asia>', 'Alias name of the cloudflare zone')
+
+    .run(async () => {
+    
+        const cfg = secret[deployer.params.zone]
+        
+        let cf = new CloudFlare(cfg.zone, cfg.email, cfg.key)
+    
+        // Set custom pages
+        // TODO: sometimes cloudflare can't fetch the html templates with error 502?
+        await cf.put('custom_pages/ratelimit_block', {
+            url: `https://cdn.redtiger.cash/error-pages/cf-error-rate-limited.html?c=` + Date.now(),
+            state: 'customized'
+        })
+        await cf.put('custom_pages/ip_block', {
+            url: `https://cdn.redtiger.cash/error-pages/cf-error-blocked.html?c=` + Date.now(),
+            state: 'customized'
+        })
+        await cf.put('custom_pages/500_errors', {
+            url: `https://cdn.redtiger.cash/error-pages/cf-error-not-available.html?c=` + Date.now(),
+            state: 'customized'
+        })
+        await cf.put('custom_pages/1000_errors', {
+            url: `https://cdn.redtiger.cash/error-pages/cf-error-not-available.html?c=` + Date.now(),
+            state: 'customized'
+        })
+        await cf.put('custom_pages/always_online', {
+            url: `https://cdn.redtiger.cash/error-pages/cf-error-not-available.html?c=` + Date.now(),
+            state: 'customized'
+        })
+    
+    
+        // Disable ipv6
+        await cf.patch('settings/ipv6', {
+            value: 'off'
+        })
+    
+        // Disable security challenge
+        await cf.patch('settings/security_level', {
+            value: 'essentially_off'
+        })
+    
+    
+        // Create Page Rule
+        let pattern = `gserver-*.${cfg.domain}/`
+    
+        // remove the same rule if exists
+        let rules = await cf.get('pagerules')
+        for (let rule of rules.result) {
+            console.log('checking rule for:', rule.targets[0].constraint)
+            if (rule.targets[0].constraint.value === pattern) {
+                console.log('deleting')
+                await cf.delete(`pagerules/${rule.id}`)
+            }
+        }
+    
+        // add the rule
+        await cf.post('pagerules', {
+            status: 'active',
+            targets: [{
+                target: 'url',
+                constraint: {operator: 'matches', value: pattern}
+            }], actions: [
+                {id: 'always_online', value: 'off'},
+                {id: 'browser_check', value: 'off'},
+                {id: 'cache_level', value: 'bypass'}
+            ]
+        })
+    
+    
+    })
