0fd5d9561ab846b0e2d0664342d9ff00fa1764d0
dichev <g.dichev@gmail.com>
2018-06-28 16:30:17 +0300

    Servers conf update script

---
+++ bin/servers-conf/update-nginx-only.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+'use strict';
+
+/**
+ * Usage:
+ * $ node bin/servers-conf/update-nginx-only --host dev-hermes-lb
+ */
+
+const installed = require('./.installed.json')
+
+let Deployer = require('deployer2')
+let deployer = new Deployer()
+deployer
+    .option('-h, --host <dev-hermes-lb|belgium-lb1>', 'The target host name (all hosts are predefined in deployer configuration)')
+   
+    .run(async () => {
+    
+        let ssh = await deployer.ssh(deployer.params.host, 'root')
+        await ssh.exec('auto-update-configs --nginx-only')
+        await ssh.disconnect()
+        
+    })
