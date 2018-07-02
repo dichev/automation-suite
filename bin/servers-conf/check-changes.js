f2d7073de9f928e783ae8db0f23ee11a5d8f2b86
dichev <g.dichev@gmail.com>
2018-07-02 11:57:30 +0300

    Servers conf: check changes script

---
+++ bin/servers-conf/check-changes.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use strict';
+
+/**
+ * Usage:
+ * $ node bin/servers-conf/update-nginx-and-webs --hosts belgium-lb
+ */
+
+const Deployer = require('deployer2')
+const HOSTS = require('configurator').hosts
+const installed = require('./.installed.json')
+
+let deployer = new Deployer()
+deployer
+    .option('-h, --hosts <list|all>', 'The target host name', {choices: installed.hosts})
+    .loop('hosts')
+    .run(async (host) => {
+        
+        let ssh = await deployer.ssh(HOSTS.get(host).ip, 'root')
+        await ssh.chdir('/opt/servers-conf')
+        await ssh.exec('git fetch origin master --quiet')
+        await ssh.exec('git log HEAD..origin/master --oneline')
+        await ssh.exec('git diff HEAD..origin/master --name-status')
+    })
