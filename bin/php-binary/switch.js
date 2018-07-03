12e7f07bb8b653d2b9dd4417ea1771fc8f2f898b
Dancho <dancho@hahpss.com>
2018-07-03 14:54:44 +0300

    last

--- bin/php-binary/switch.js
+++ bin/php-binary/switch.js
@@ -8,18 +8,22 @@
 
 
 let Deployer = require('deployer2')
-let deployer = new Deployer()
+const cfg = require('configurator')
+const installed = require('./.installed.json')
+
+let deployer = new Deployer(cfg.devops)
 
 
 deployer
-    .option('-h, --host <name>', 'The target host name (all hosts are predefined in deployer configuration)')
-    .option('-p --php-version NUMBER') /* ne sam siguren dali -v i --version e nau-udachnoto */
+    .option('-h, --hosts <list|all>', 'The target host name', {choices: installed.hosts})
+    .option('-p, --phpversion NUMBER') 
+    .loop('hosts')
    
     .run(async () => {
-    	console.info('Switching php version to:'+deployer.params.version)
-        let ssh = await deployer.ssh(deployer.params.host, 'root')
+    	console.info('Switching php version to:'+deployer.params.phpversion)
+	let ssh = await deployer.ssh(cfg.hosts.get(host).ip, 'root')
         await ssh.exec('rm /opt/phpbrew/php/php')
-        await ssh.exec('ln -s /opt/phpbrew/php/php'+ deployer.params.version + ' /opt/phpbrew/php/php')
+        await ssh.exec('ln -s /opt/phpbrew/php/php'+ deployer.params.phpversion + ' /opt/phpbrew/php/php')
         console.info('Done')
     })
 
