0a855b3b0f2a214d42a6c130a4effea5cbaea291
Dancho <dancho@hahpss.com>
2018-07-04 14:28:05 +0300

    fpm cnahges

--- bin/php-binary/fpm.js
+++ bin/php-binary/fpm.js
@@ -7,16 +7,25 @@
  */
 
 
-let Deployer = require('deployer2')
-let deployer = new Deployer()
 
+const Deployer = require('deployer2')
+const installed = require('./.installed.json')
+const cfg = require('configurator')
+let deployer = new Deployer(cfg.devops)
 
 deployer
-    .option('-h, --host <name>', 'The target host name (all hosts are predefined in deployer configuration)')
-    .option('-a, --action start, restart or stop') /* nqkakwa proverka da priema samo trite stoinosti */
-   
-    .run(async () => {
-        let ssh = await deployer.ssh(deployer.params.host, 'root')
-        await ssh.exec('/etc/init.d/php-fpm '+ deployer.params.action)
+
+    .option('-h, --hosts <list|all>', 'The target host name', {choices: installed.hosts})
+    .option('-a, --action <action>', 'Use start, restart, stop')
+    .loop('hosts')
+
+    .run(async (host) => {
+        let ssh = await deployer.ssh(cfg.hosts.get(host).ip, 'root')
+	try {
+          await ssh.exec('/etc/init.d/php-fpm '+ deployer.params.action)
+        } catch  (err) {
+          deployer.chat.notify('php-fpm dont ' + deployer.params.action ' on '  + host, {color: 'red'})
+        }
+
     })
 
