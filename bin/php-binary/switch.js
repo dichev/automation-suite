042aca9f95df7c5d51b6ce08c35b1f9e9680976e
Dancho <dancho@hahpss.com>
2018-07-04 11:07:57 +0300

    changes

--- bin/php-binary/switch.js
+++ bin/php-binary/switch.js
@@ -3,27 +3,27 @@
 
 /**
  * Usage:
- * $ node bin/sys-metrics/swith --host dev-hermes-web1 --version  7.1.9
+ * $ node bin/php-binary/swith --host dev-hermes-web1 --version  7.1.9
  */
 
 
-let Deployer = require('deployer2')
-const cfg = require('configurator')
+const Deployer = require('deployer2')
 const installed = require('./.installed.json')
-
+const cfg = require('configurator')
 let deployer = new Deployer(cfg.devops)
 
-
 deployer
+
     .option('-h, --hosts <list|all>', 'The target host name', {choices: installed.hosts})
-    .option('-p, --phpversion NUMBER') 
+    .option('-p, --phpversion <version>', 'The php version number', {choices: installed.versions, def: '7.1.19'})
     .loop('hosts')
-   
-    .run(async () => {
-    	console.info('Switching php version to:'+deployer.params.phpversion)
+
+    .run(async (host) => {
+    	console.info('Switching php version to: '+deployer.params.phpversion)
 	let ssh = await deployer.ssh(cfg.hosts.get(host).ip, 'root')
         await ssh.exec('rm /opt/phpbrew/php/php')
         await ssh.exec('ln -s /opt/phpbrew/php/php'+ deployer.params.phpversion + ' /opt/phpbrew/php/php')
         console.info('Done')
+	deployer.chat.notify('php version switched to '+ deployer.params.phpversion + ' on ' + host, {color: 'green'})
     })
 
