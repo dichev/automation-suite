0ee8226296a64c41fab025deb4ef705ca5e3f46c
Dancho <dancho@hahpss.com>
2018-07-10 11:53:29 +0300

    skip errors

--- bin/php-binary/init.js
+++ bin/php-binary/init.js
@@ -40,10 +40,9 @@ deployer
         await ssh.exec('rm /opt/phpbrew/php/php/etc/php.ini && ln -s /opt/servers-conf/php/php.ini /opt/phpbrew/php/php/etc/php.ini')
         await ssh.exec('rm /usr/bin/php && ln -s /opt/phpbrew/php/php/bin/php /usr/bin/php')
         await ssh.exec('rm /etc/init.d/php*-fpm && ln -s /opt/servers-conf/php/php-fpm.init.d /etc/init.d/php-fpm')
-        await ssh.exec('rm /lib/systemd/system/php*-fpm.service && systemctl enable /opt/servers-conf/php/php-fpm.service')
+        await ssh.exec('rm /lib/systemd/system/php*-fpm.service || systemctl enable /opt/servers-conf/php/php-fpm.service')
         await ssh.exec('rm /etc/logrotate.d/php*-fpm && ln -s /opt/servers-conf/php/logrotate /etc/logrotate.d/php-fpm')
-        //await ssh.exec('sleep 2; killall -9 php-fpm')
-	await ssh.exec('sleep 2; killall -9 php5-fpm')
+        await ssh.exec('sleep 2; killall -9 php-fpm || killall -9 php5-fpm || true')
         await ssh.exec('systemctl restart php-fpm') //system
         
         await deployer.exec(`node bin/php-binary/check --hosts ${host}`)
