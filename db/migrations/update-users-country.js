a1f3719290f755658e09fa84d27a04ffabbe5918
dichev <g.dichev@gmail.com>
2018-08-05 15:45:13 +0300

    Optimize in chunks

--- db/migrations/update-users-country.js
+++ db/migrations/update-users-country.js
@@ -14,12 +14,12 @@ const SQL_SELECT = `
         (SELECT if(countryCode = '--', 'XX', countryCode) FROM ipguard_ranges WHERE ipTo >= u.ip LIMIT 1) as country
     from users u
     where id > ? and (country is NULL or country = '--')
-    order by id asc limit 1
+    order by id asc limit ?
 `
 // 2. Update data
-const SQL_UPDATE = `
-    UPDATE users SET country = ? WHERE (country is NULL or country = '--') AND id = ?;
-`
+// const SQL_UPDATE = `
+//     UPDATE users SET country = ? WHERE id = ?;
+// `
 
 
 const Program = require('dopamine-toolbox').Program
@@ -31,9 +31,11 @@ let program = new Program({ chat: cfg.chat.rooms.devops })
 program
     .description(`Update users country using ip geolocation. This is very expensive migration, that's why is executed in a loop user by user`)
     .option('-o, --operators <name>', 'The target operator name', { required: true, choices: Object.keys(cfg.operators) })
+    .option('--chunk-size <int>', 'How many user to be calculated together', { def: 20 })
 
     .iterate('operators', async (operator) => {
         const LOCATION = cfg.operators[operator].location
+        const CHUNK_SIZE = parseInt(program.params.chunkSize) || 10
         
         let hosts = cfg.locations[LOCATION].hosts
         let master = await program.mysql({user: 'root', ssh: {user: 'root', host: hosts.mysql}})
@@ -53,15 +55,24 @@ program
         let lastId = 0
         let i = 0
         while(true) {
-            let [row] = await master.query(SQL_SELECT, [lastId])
-            if(!row) break
-            let {id, country} = row
-            if(!country || !id) throw Error(`Unexpected values` + {id, country})
-            
-            let res = await master.query(SQL_UPDATE, [country, id])
-            console.log(`${operator} | ${++i} / ${expected}: Update user #${id} to ${country} (${res.info})`)
-            lastId = id
-            
+            let rows = await master.query(SQL_SELECT, [lastId, CHUNK_SIZE])
+            if(!rows.length) break
+    
+            let sqlUpdate = ''
+            for(let row of rows) {
+                let {id, country} = row
+                if (!country || !id) throw Error(`Unexpected values` + {id, country})
+                
+                sqlUpdate += `UPDATE users SET country = '${country}' WHERE id = ${id};\n`
+                lastId = id
+                ++i
+            }
+            if(!sqlUpdate) throw Error('Something is wrong, check here')
+            // console.log(sqlUpdate)
+            let res = await master.query(sqlUpdate)
+            console.log(`${operator} | ${i} / ${expected}: Updated users #${rows[0].id}..#${rows[rows.length-1].id}`)
+    
+    
             if(i === 10 || i % 1000 === 0) program.chat.notify(`${operator} | Updated ${i} / ${expected} users`)
         }
         
