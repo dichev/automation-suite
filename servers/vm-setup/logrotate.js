91b64f794281c92d01e9c40bb404ecbae7c076b4
dichev <g.dichev@gmail.com>
2018-08-28 14:44:31 +0300

    Logrotate - support only validation

--- servers/vm-setup/logrotate.js
+++ servers/vm-setup/logrotate.js
@@ -10,33 +10,36 @@ let program = new Program({ chat: cfg.chat.rooms.devops })
 program
     .description('Setup logrotate configurations')
     .option('-h, --hosts <list>', 'The target host names', { choices: Object.keys(cfg.hosts), required: true })
+    .option('--only-validate', 'Perform just validation of the current logrotate configuration')
     
     .iterate('hosts', async (host) => {
         let ssh = await program.ssh(cfg.getHost(host).ip, 'root')
         const type = cfg.getHost(host).type
         
-        switch (type){
-            case 'web':
-                await ssh.exec('ln -svf /opt/servers-conf/logrotate/php-fpm /etc/logrotate.d/php-fpm')
-                await ssh.exec('ln -svf /opt/servers-conf/logrotate/phperror /etc/logrotate.d/phperror')
-                await ssh.exec('ln -svf /opt/servers-conf/logrotate/rsyslog /etc/logrotate.d/rsyslog')
-                break
-    
-            case 'mysql':
-            case 'mysql-archive':
-            case 'mysql-slave':
-            case 'mysql-master':
-                await ssh.exec('ln -svf /opt/servers-conf/logrotate/mysql /etc/logrotate.d/mysql')
-                await ssh.exec('ln -svf /opt/servers-conf/logrotate/rsyslog /etc/logrotate.d/rsyslog')
-                break
-                
-            case 'lb':
-                await ssh.exec('ln -svf /opt/servers-conf/logrotate/nginx /etc/logrotate.d/nginx')
-                await ssh.exec('ln -svf /opt/servers-conf/logrotate/rsyslog /etc/logrotate.d/rsyslog')
-                break
-            
-            default:
-                throw Error('Unexpected host type:' + type)
+        if(!program.params.onlyValidate) {
+            switch (type) {
+                case 'web':
+                    await ssh.exec('ln -svf /opt/servers-conf/logrotate/php-fpm /etc/logrotate.d/php-fpm')
+                    await ssh.exec('ln -svf /opt/servers-conf/logrotate/phperror /etc/logrotate.d/phperror')
+                    await ssh.exec('ln -svf /opt/servers-conf/logrotate/rsyslog /etc/logrotate.d/rsyslog')
+                    break
+        
+                case 'mysql':
+                case 'mysql-archive':
+                case 'mysql-slave':
+                case 'mysql-master':
+                    await ssh.exec('ln -svf /opt/servers-conf/logrotate/mysql /etc/logrotate.d/mysql')
+                    await ssh.exec('ln -svf /opt/servers-conf/logrotate/rsyslog /etc/logrotate.d/rsyslog')
+                    break
+        
+                case 'lb':
+                    await ssh.exec('ln -svf /opt/servers-conf/logrotate/nginx /etc/logrotate.d/nginx')
+                    await ssh.exec('ln -svf /opt/servers-conf/logrotate/rsyslog /etc/logrotate.d/rsyslog')
+                    break
+        
+                default:
+                    throw Error('Unexpected host type:' + type)
+            }
         }
         
         
