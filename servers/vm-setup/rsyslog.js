c68e3e80fbe46c852913a8ce1aa9024da9279b0c
dichev <g.dichev@gmail.com>
2018-10-19 19:21:57 +0300

    Rsyslog: symlink validations

--- servers/vm-setup/rsyslog.js
+++ servers/vm-setup/rsyslog.js
@@ -6,6 +6,7 @@ const Program = require('dopamine-toolbox').Program
 const Tester = require('dopamine-toolbox').Tester
 const cfg = require('configurator')
 let program = new Program({ chat: cfg.chat.rooms.devops })
+let tester = new Tester()
 
 
 program
@@ -16,41 +17,46 @@ program
     .iterate('hosts', async (host) => {
         let ssh = await program.ssh(cfg.getHost(host).ip, 'root')
         const type = cfg.getHost(host).type
-        
-        if(!program.params.onlyValidate) {
-            
-            switch (type) {
-                case 'web':
-                    await ssh.exec('ln -svf /opt/servers-conf/rsyslog/12-php.conf /etc/rsyslog.d/12-php.conf && [ -f /etc/rsyslog.d/12-php.conf ]')
-                    break
-        
-                case 'mysql':
-                case 'mysql-archive':
-                case 'mysql-slave':
-                case 'mysql-master':
-                    await ssh.exec('ln -svf /opt/servers-conf/rsyslog/10-mysql.conf /etc/rsyslog.d/10-mysql.conf && [ -f /etc/rsyslog.d/10-mysql.conf ]')
-                    break
-        
-                case 'lb':
-                    await ssh.exec('ln -svf /opt/servers-conf/rsyslog/11-nginx.conf /etc/rsyslog.d/11-nginx.conf && [ -f /etc/rsyslog.d/11-nginx.conf ]')
-                    break
-        
-                default:
-                    throw Error('Unexpected host type:' + type)
-            }
     
-            await ssh.exec('ln -svf /opt/servers-conf/rsyslog/common.conf /etc/rsyslog.d/common.conf && [ -f /etc/rsyslog.d/common.conf ]')
-        }
+        let it = tester.it
+        it(`php config exists`, async () => await ssh.exists(`/opt/servers-conf/rsyslog/12-php.conf`))
+        it(`mysql config exists`, async () => await ssh.exists(`/opt/servers-conf/rsyslog/10-mysql.conf`))
+        it(`nginx config exists`, async () => await ssh.exists(`/opt/servers-conf/rsyslog/11-nginx.conf`))
+        it(`common config exists`, async () => await ssh.exists(`/opt/servers-conf/rsyslog/common.conf`))
+        it(`validate rsyslog configuration`, async () => await ssh.exec(`rsyslogd -N1`, {silent: true}))
+        await tester.run(true)
         
-        let tester = new Tester()
-        tester.it(`validate rsyslog configuration`, async () => await ssh.exec(`rsyslogd -N1`, {silent: true}))
+        if(program.params.onlyValidate) return
+        
+        
+        switch (type) {
+            case 'web':
+                await ssh.exec('ln -svf /opt/servers-conf/rsyslog/12-php.conf /etc/rsyslog.d/12-php.conf')
+                break
+    
+            case 'mysql':
+            case 'mysql-archive':
+            case 'mysql-slave':
+            case 'mysql-master':
+                await ssh.exec('ln -svf /opt/servers-conf/rsyslog/10-mysql.conf /etc/rsyslog.d/10-mysql.conf')
+                break
+    
+            case 'lb':
+                await ssh.exec('ln -svf /opt/servers-conf/rsyslog/11-nginx.conf /etc/rsyslog.d/11-nginx.conf')
+                break
+    
+            default:
+                throw Error('Unexpected host type:' + type)
+        }
+
+        await ssh.exec('ln -svf /opt/servers-conf/rsyslog/common.conf /etc/rsyslog.d/common.conf && [ -f /etc/rsyslog.d/common.conf ]')
+      
+        it(`validate rsyslog configuration`, async () => await ssh.exec(`rsyslogd -N1`, {silent: true}))
         await tester.run(true)
     
-        if (!program.params.onlyValidate) {
-            await program.confirm('Do you want to load the configurations?')
-            await ssh.exec('systemctl restart rsyslog')
-            await ssh.exec('sleep 1 && systemctl status rsyslog | head -n 3')
-        }
+        await program.confirm('Do you want to load the configurations?')
+        await ssh.exec('systemctl restart rsyslog')
+        await ssh.exec('sleep 1 && systemctl status rsyslog | head -n 3')
         
         console.log('Success')
     })
