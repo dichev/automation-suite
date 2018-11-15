b4c112dd9f1296081b476982994cb165cd95c3c9
dichev <g.dichev@gmail.com>
2018-11-15 09:48:53 +0200

    Mysql update conf

---
+++ servers/mysql-conf/update.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+'use strict';
+
+const Program = require('dopamine-toolbox').Program
+const SSHClient = require('dopamine-toolbox').SSHClient
+const cfg = require('configurator')
+const DB_HOSTS = Object.keys(cfg.hosts).filter(h => h.includes('sql') || h.includes('-db-'))
+
+let program = new Program({ chat: cfg.chat.rooms.devops })
+program
+    .description('Setup unified mysql configuration')
+    .option('-h, --hosts <list|all>', 'The target host name', {choices: DB_HOSTS, required: true})
+
+    .iterate('hosts', async (host) => {
+        let ssh = await new SSHClient().connect({host: cfg.getHost(host).ip, username: 'root'})
+        
+        
+        await program.chat.message('Fetching changes')
+        
+        await ssh.chdir('/opt/servers-conf-mysql')
+        await ssh.exec('git status')
+        await ssh.exec('git log HEAD..origin/master --oneline')
+        await ssh.exec('git diff HEAD..origin/master --name-status')
+        
+        await program.confirm('Do you want to pull the changes?')
+        await ssh.exec(`git pull --no-rebase --ff-only --prune`)
+        // await ssh.exec(`git diff`);
+        
+        let answer = await program.ask('Do you want to restart mysql', ['yes', 'no'], 'no')
+        if(answer === 'yes') {
+            await program.chat.message('Restarting mysql')
+            console.log('Restarting mysql while watching the error log, press ctrl+c to end')
+            ssh.exec(`tail -f /var/log/mysql/error.log`).catch(console.error);
+            await ssh.exec(`/etc/init.d/mysql restart`)
+        }
+        
+        console.log('Ready')
+        
+        // await ssh.disconnect()
+})
+
