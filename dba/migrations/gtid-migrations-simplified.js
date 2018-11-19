189d0bd42a572f3f839f5c1d7cb085538ae38b29
Nikolay Terziev <n.terziev@dopamine.bg>
2018-11-19 12:00:08 +0200

    GTID migrate

---
+++ dba/migrations/gtid-migrations-simplified.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+'use strict'
+
+// source https://dev.mysql.com/doc/refman/5.7/en/replication-mode-change-online-enable-gtids.html
+// usage ./gtid-migrations-simplified.js --databases [belgium1,dev,etc] --group [master,archive]
+
+const   Program = require('dopamine-toolbox').Program,
+        SSHClient = require('dopamine-toolbox').SSHClient,
+        MySQL = require('dopamine-toolbox').MySQL,
+        cfg = require('configurator'),
+        DB_CNF = {
+            user: 'root',//cfg.access.mysql.readOnly.user,
+            password: '',//cfg.access.mysql.readOnly.password,
+            database:''
+        },
+        CMD_GTID_OFF = `cd /etc/mysql && git reset --hard GTID_OFF_2 && git status`,
+        CMD_GTID_ON  = `cd /etc/mysql && git reset --hard GTID_ON_2 && git status`;
+let     sshConnections = {},
+        dbConnections = {},
+        program = new Program();
+
+let run = async (what,where,payload,cb = () => {}) => { // replace ['SELECT NOW() as tm, "OFF" as Value' ... ] with [p] for production purposes ---\/
+    await program.sleep(2,payload.join(', ') + ' -> ' + where.join(', '))
+    await program.confirm("Confirm to continue:")
+    if(what === 'query') for(let ip of where) for (let p of payload) cb(ip,await dbConnections[ip].query(p))
+    if(what === 'exec')  for(let ip of where) for (let p of payload) cb(ip,await sshConnections[ip].exec(p)) // <-- replace ['whoami'] with [p] for production purposes
+}
+let checkMode = (ip,res) => {
+    for(let line of res){
+        if(line.Value === 'ON') throw(ip + ' ' + line.Variable_name + ' is set to: ' + line.Value)
+    }
+}
+
+program
+    .description("Migrate to GTID")
+    .option('-d, --databases <name>', 'Target database from Configurator.databases', { required: true, choices: Object.keys(cfg.databases) })
+    .option('-g, --group <name>',' Target cluster group [master,archive]',{ required:true, choices: ['master','archive']})
+    .iterate('databases', async (db) => {
+
+        let hostsAll = [cfg.databases[db].master,cfg.databases[db].backups.master,cfg.databases[db].slave],
+            hostsSlave = [cfg.databases[db].backups.master,cfg.databases[db].slave];
+        if(program.params.group === 'archive'){
+            hostsAll = [cfg.databases[db].archive,cfg.databases[db].backups.archive]
+            hostsSlave = [cfg.databases[db].backups.archive]
+        }
+        await program.confirm("Processing:[" + hostsAll.join(', ') + "] with slaves: [" + hostsSlave.join(', ') + '] ?')
+        for(let host of hostsAll){
+            sshConnections[host] = await new SSHClient().connect({host: host,username: 'root',agent:process.env.SSH_AUTH_SOCK})
+            dbConnections[host] = await new MySQL().connect(DB_CNF, sshConnections[host])
+        }
+
+        await run('query',hostsAll,["SHOW VARIABLES LIKE '%gtid_mod%'"],checkMode)
+
+        await program.confirm("Look above. Do you want to continue!")
+        await run('exec',hostsAll,[CMD_GTID_OFF])
+
+        await program.confirm("Ini files modified. Please check. Pres enter when ready!")
+
+        await run('query',hostsAll,["SET @@GLOBAL.ENFORCE_GTID_CONSISTENCY = WARN"])
+
+        await program.confirm("Leave running for a while and check logs for any warnings. Continue (if no warnings) !")
+        await run('query',hostsAll,["SET @@GLOBAL.ENFORCE_GTID_CONSISTENCY = ON"])
+        await run('query',hostsAll,["SET @@GLOBAL.GTID_MODE = OFF_PERMISSIVE"])
+        await run('query',hostsAll,["SET @@GLOBAL.GTID_MODE = ON_PERMISSIVE"])
+
+        await run('query',hostsAll,["SHOW STATUS LIKE 'ONGOING_ANONYMOUS_TRANSACTION_COUNT'"],console.log)
+        await program.confirm("On each server, wait until the status variable ONGOING_ANONYMOUS_TRANSACTION_COUNT is zero. \nQUERY: SHOW STATUS LIKE 'ONGOING_ANONYMOUS_TRANSACTION_COUNT'")
+        await run('query',hostsAll,["SET @@GLOBAL.GTID_MODE = ON"])
+        await run('exec',hostsAll,[CMD_GTID_ON])
+
+        await program.confirm("Ini files modified. Please check. Pres enter when ready!")
+/*
+        await run('query',hostsSlave,[
+            "STOP SLAVE [FOR CHANNEL 'channel']",
+            "CHANGE MASTER TO MASTER_AUTO_POSITION = 1 [FOR CHANNEL 'channel']",
+            "START SLAVE [FOR CHANNEL 'channel']"
+        ])
+*/
+        await run('query',hostsSlave,[
+            "STOP SLAVE",
+            "CHANGE MASTER TO MASTER_AUTO_POSITION = 1",
+            "START SLAVE"
+        ])
+
+        await run('query',hostsAll,["SHOW VARIABLES LIKE '%relay_log_basename%'"],console.log)
+/**/
+        for(let ip in dbConnections) await dbConnections[ip].disconnect()
+        for(let ip in sshConnections) await sshConnections[ip].disconnect()
+        console.log('done');
+});
+
+/*
+watch "cd /etc/mysql && git status"
+watch "mysql -e \"SHOW VARIABLES LIKE '%gtid%'\" | column -t"
+tail -f /var/log/mysql/error.log
+ */
---
+++ dba/migrations/operations-by-location.js
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+'use strict'
+
+const   Program = require('dopamine-toolbox').Program,
+        SSHClient = require('dopamine-toolbox').SSHClient,
+        MySQL = require('dopamine-toolbox').MySQL,
+        colors = require('ansicolors'),
+        cfg = require('configurator');
+
+let     sshConnections = {},
+        dbConnections = {},
+        allowedLocations = [],
+        program = new Program();
+
+// parse config to get list of allowed/available locations
+
+for(let host in cfg.hosts){
+    let l = cfg.hosts[host].location
+    if(allowedLocations.indexOf(l) === -1 && l !== '') allowedLocations.push(l)
+}
+
+let run = async (what,where,payload,cb = () => {}) => {
+    await program.sleep(2,payload.join(', ') + ' -> ' + Object.keys(where).join(', '))
+    if(what === 'query') for(let hostName in where) for (let p of payload) cb([hostName,await dbConnections[hostName].query(p)])
+    if(what === 'exec')  for(let hostName in where) for (let p of payload) cb([hostName,await sshConnections[hostName].exec(p,{silent:true})])
+}
+
+let connectSSH = async (hosts) => {
+    for(let hostName in hosts){
+        try{
+            sshConnections[hostName] = await new SSHClient().connect({host: hosts[hostName].ip,username: 'root'})
+        }catch(e){ console.log(['SSH',hostName,e.message]) }
+    }
+}
+
+let connectDba = async (hosts) => {
+    for(let hostName in hosts){
+        try{
+            if(typeof sshConnections[hostName] !== 'undefined'){ //  connect only hosts with active ssh tunel
+                dbConnections[hostName] = await new MySQL().connect({user:'root'}, sshConnections[hostName])
+            }
+        }catch(e){ console.log(['MySQL',hostName,e.message]) }
+    }
+}
+
+program
+    .description("Migrated Operator settings")
+    .option('-l, --locations <name>', 'targetLocation', { required: true, choices: allowedLocations })
+    .iterate('locations', async (location) => {
+        let hosts = {}
+        let hostsDbArchive = {}
+        let hostsSSh = {}
+        let hostsDbMaster = {}
+        let hostsWeb = {}
+        let hostsLb = {}
+
+        for (let hostName in cfg.hosts){
+            let host = cfg.hosts[hostName]
+            let type = host.type
+            if (host.location === location){
+                hosts[hostName] = host
+                if(['mysql-master','mysql-slave'].indexOf(type) > -1) hostsDbMaster[hostName] = host
+                if(['mysql-archive'].indexOf(type) > -1) hostsDbArchive[hostName] = host
+                if(['web'].indexOf(type) > -1) hostsWeb[hostName] = host
+                if(['lb'].indexOf(type) > -1) hostsLb[hostName] = host
+            }
+        }
+
+        await program.confirm("List of hosts is as fallows:\n"
+            +"HostsLb: " + colors.green(Object.keys(hostsLb).join(', ')) + "\n"
+            +"HostsWeb: " + colors.green(Object.keys(hostsWeb).join(', ')) + "\n"
+            +"HostsDbMaster: " + colors.green(Object.keys(hostsDbMaster).join(', ')) + "\n"
+            +"HostsDbArchive: " + colors.green(Object.keys(hostsDbArchive).join(', ')) + "\n"
+            +"\n"
+            +"If list is correct press ENTER to continue.\nIf list is NOT correct type [no] to exit.")
+
+        await connectSSH(hosts)
+        await connectDba(hostsDbMaster)
+        await connectDba(hostsDbArchive)
+        for(let hostName in sshConnections) hostsSSh[hostName] = hosts[hostName]
+
+/*
+...
+    Code goes here. List of useful variables!!!
+
+    hosts => All hosts for that location.
+    hostsDbMaster => All Databases from Master Block for this location.
+    hostsDbArchive => All Databases from Archive Block for this location.
+    hostsWeb => All Web ( php ) hosts for that location.
+    hostsLb => All lb's for that location.
+    hostsSSH => All hosts connected over ssh.
+
+... EXAMPLES Below ---\/
+*/
+
+        await run('query',hostsDbMaster,["SELECT NOW() as tm"],console.log)
+        await run('query',hostsDbArchive,["SELECT NOW() as tm"],console.log)
+        await run('exec',hostsWeb,[
+            "whoami",
+            "su dopamine -c \"cd /home/dopamine && find /home/dopamine -name server.config.php -exec grep \\\"host\\\" '{}' \\; | wc -l \"",
+            "date +%Y-%M-%d\\ %H:%m:%S"
+        ],console.log)
+
+/*
+... EXAMPLES Above ---/\
+
+*/
+
+        for(let ip in dbConnections) await dbConnections[ip].disconnect()
+        for(let ip in sshConnections) await sshConnections[ip].disconnect()
+        console.log('done');
+    });
