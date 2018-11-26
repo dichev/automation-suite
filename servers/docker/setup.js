669c3b13a5bed34be65328c690513ea971af8f41
dichev <g.dichev@gmail.com>
2018-11-26 19:17:44 +0200

    Docker setup: Simplify by host

--- servers/docker/setup.js
+++ servers/docker/setup.js
@@ -12,38 +12,31 @@ let program = new Program({ chat: cfg.chat.rooms.devops })
 program
     .icon(GoogleChat.icons.DEPLOY)
     .description('Setup docker on all web instances')
-    .option('-l, --locations <list|all>', 'The target location name', {choices: Object.keys(cfg.locations), required: true})
+    .option('-h, --hosts <list|all>', 'The target host name', {choices: Object.keys(cfg.hosts), required: true})
 
-    .iterate('locations', async (location) => {
+    .iterate('hosts', async (host) => {
         console.log('Installing docker-ce according their manual:\n https://docs.docker.com/install/linux/docker-ce/debian/#install-docker-ce')
         
-        let webs = []
-        for (let web of cfg.locations[location].hosts.webs) {
-            if(web.ip === '10.132.23.111') continue
-            webs.push(await new SSHClient().connect({host: web.ip, username: 'root'}))
-        }
+        let web = await new SSHClient().connect({host: cfg.getHost(host).ip, username: 'root'})
+        
     
         console.log('Installing packages..')
-        for(let web of webs){
-            await web.exec(`
-                set -e
-                apt-get -q update
-                apt-get -q -y install software-properties-common
-                
-                curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-                add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-                
-                apt-get -q update
-                apt-get -q -y install docker-ce
-            `)
-        }
+        await web.exec(`
+            set -e
+            apt-get -qq update
+            apt-get -q -y install software-properties-common
+            
+            curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+            add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+            
+            apt-get -qq update
+            apt-get -q -y install docker-ce
+        `)
 
         
-        console.log('Checking docker version')
-        for (let web of webs) {
-            await web.exec('docker -v')
-            await web.disconnect()
-        }
+        console.log('\n\nChecking docker version')
+        await web.exec('docker -v')
+        await web.disconnect()
         
     })
 
