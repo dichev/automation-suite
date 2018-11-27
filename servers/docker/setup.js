3b152f0f4f64d9eeb9f3c179d35d715b0f413b77
dichev <g.dichev@gmail.com>
2018-11-27 10:25:17 +0200

    Docker setup: protect from reinstall (could be container destroyable)

--- servers/docker/setup.js
+++ servers/docker/setup.js
@@ -3,14 +3,12 @@
 
 const Program = require('dopamine-toolbox').Program
 const SSHClient = require('dopamine-toolbox').SSHClient
-const GoogleChat = require('dopamine-toolbox').plugins.GoogleChat
 const cfg = require('configurator')
 
 let program = new Program({ chat: cfg.chat.rooms.devops })
 
 
 program
-    .icon(GoogleChat.icons.DEPLOY)
     .description('Setup docker on all web instances')
     .option('-h, --hosts <list|all>', 'The target host name', {choices: Object.keys(cfg.hosts), required: true})
 
@@ -18,9 +16,19 @@ program
         console.log('Installing docker-ce according their manual:\n https://docs.docker.com/install/linux/docker-ce/debian/#install-docker-ce')
         
         let web = await new SSHClient().connect({host: cfg.getHost(host).ip, username: 'root'})
+    
+    
+        console.log('Checking for docker:')
+        try {
+            await web.exec('docker -v')
+            console.warn('Docker is already installed, skipping..')
+            return await web.disconnect()
+        } catch (err) {
+            // that's fine
+        }
         
     
-        console.log('Installing packages..')
+        console.log('\nInstalling packages..')
         await web.exec(`
             set -e
             apt-get -qq update
