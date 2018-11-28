975a404aa9e7e06bfd5174ed2ab2fce3ac861cfc
dichev <g.dichev@gmail.com>
2018-11-28 12:29:06 +0200

    Mysql conf: explicitly require 'restart' as parameter to avoid mistakes

--- servers/mysql-conf/update.js
+++ servers/mysql-conf/update.js
@@ -10,6 +10,7 @@ let program = new Program({ chat: cfg.chat.rooms.devops })
 program
     .description('Setup unified mysql configuration')
     .option('-h, --hosts <list|all>', 'The target host name', {choices: DB_HOSTS, required: true})
+    .option('--mode <restart|fetch>', 'Restart mysql server or just fetch the changes (will be applied on next restart)', {choices: ['restart', 'fetch']})
     .option('--reset', 'Reset manual changes')
 
     .iterate('hosts', async (host) => {
@@ -20,7 +21,7 @@ program
         if (changes) {
             await ssh.exec(`git diff`)
             if(program.params.reset) {
-                await program.chat.message('Reset manual changes?')
+                await program.confirm('Reset manual changes?')
                 await ssh.exec('git reset --hard')
             } else {
                 throw Error(`Aborting.. Manual changes found (use --reset to clean them)`)
@@ -34,15 +35,21 @@ program
     
         await program.confirm('Do you want to pull the changes?')
         await ssh.exec(`git pull --no-rebase --ff-only --prune`)
-        
-        let answer = await program.ask('DANGEROUS! Do you want to restart mysql', ['yes', 'no'])
-        if(answer === 'yes') {
-            await program.chat.message(' Restarting mysql..')
-            console.log('Restarting mysql while watching the error log, press ctrl+c to end')
-            ssh.exec(`tail -f /var/log/mysql/error.log`).catch(console.error);
-            await ssh.exec(`/etc/init.d/mysql restart`)
+    
+        if(program.params.mode === 'restart') {
+            const isSlave = cfg.hosts[host].type === 'mysql-slave'
+            let answer = isSlave ? 'yes' : await program.ask(`DANGEROUS! Do you really want to restart ${cfg.hosts[host].type}`, ['yes', 'no'])
+            if (answer === 'yes') {
+                await program.chat.message(' Restarting mysql..')
+                console.log('Restarting mysql while watching the error log, press ctrl+c to end')
+                ssh.exec(`tail -f /var/log/mysql/error.log`).catch(console.error);
+                await ssh.exec(`/etc/init.d/mysql restart`)
+                await program.sleep(5)
+            }
         }
-        
+        else {
+            console.warn(`WARNING! The changes will be applied on next restart. You should set them now using SET GLOBAL`)
+        }
         console.log('Ready')
         
         // await ssh.disconnect()
