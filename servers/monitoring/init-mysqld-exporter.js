d6a7886291072701bc377420963370a3b8875fe2
plamen <p.yonchev@dopamine.bg>
2018-12-18 15:54:00 +0200

    Parse params and filter by network

--- servers/monitoring/init-mysqld-exporter.js
+++ servers/monitoring/init-mysqld-exporter.js
@@ -35,13 +35,16 @@ const IP8 = '192.168.110.66';
 const devQAMySQLIP = '192.168.14.31';
 
 program
-.description('Setup monitoring mysql exporter')
+.description('Setup monitoring: Mysqld Exporter')
 .option('-h, --hosts <list|all>', 'The target host names', { choices: Object.keys(cfg.hosts), required: true })
-.option('-n, --networks <list|all>', 'Networks', {required: true})
+.option('-n, --networks <list|all>', 'Networks', { choices: [... new Set(Object.values(cfg.hosts).map(i => i.network))], required: true})
 .option('-f, --force', 'Skip manual changes validations and proceed on your risk')
+.parse()
 
+// filter by network
+program.params.hosts = Object.values(cfg.hosts).filter(h => program.params.networks.includes(h.network)).map(i => i.name).join(',')
 
-.iterate('hosts', async (host) => {
+program.iterate('hosts', async (host) => {
     const params = program.params
     const force  = params.force !== undefined;
     let networks  = params.networks !== undefined ? params.networks : null;
@@ -49,110 +52,108 @@ program
 
     let hostNetwork = cfg.getHost(host).network;
 
-    // TODO: This will print that's executing on host, but it's NOT. Can it be fixed in someway
     // Execute only if the host network is included
-    if (networks.includes(hostNetwork)) { // TODO: има ли начин тук да кажа continue, вместо да ограждам целият код със скоби
-        let hostIP = cfg.getHost(host).ip;
+    if (!networks.includes(hostNetwork)) return;
 
-        console.log(`Starting script on HOST:(${host} : ${hostIP})...`)
-        await program.chat.notify(`Starting script on HOST:(${host} : ${hostIP})...`)
+    let hostIP = cfg.getHost(host).ip;
 
-        let ssh = await program.ssh(cfg.getHost(host).ip, 'root')
+    console.log(`Starting script on HOST:(${host} : ${hostIP})...`)
+    await program.chat.notify(`Starting script on HOST:(${host} : ${hostIP})...`)
 
-        let checkNetToolsInstalled = await ssh.exec(`dpkg -l | grep nettools > /dev/null 2>&1 && echo '1' || echo '0'`)
-        if (!checkNetToolsInstalled) {
-            // Install net-tools
-            await ssh.exec('apt-get install -y net-tools > /dev/null')
-        }
+    let ssh = await program.ssh(cfg.getHost(host).ip, 'root')
 
-        // Skip host if mysql is not installed
-        let mysqlPortInUse = await ssh.exec(`netstat -tulpn | grep LISTEN | grep ${MYSQL_PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
-        if (mysqlPortInUse === '0') return;
-
-        let portIsInUse = await ssh.exec(`netstat -tulpn | grep LISTEN | grep ${PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
-        let iptablesRulesExist = await ssh.exec(`iptables -L | awk -F ':' ' {print $2}' | sort -u | grep ${PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
-
-        if (!force && (portIsInUse === '1' && iptablesRulesExist === '1')) {
-            await program.chat.notify('Skipping host')
-        }
-        else {
-            // Install
-            await ssh.exec('rm -rfv /opt/mysqld_exporter') //temp
-            let optMysqlNodeExporter = '/opt/mysqld_exporter'
-            await program.chat.notify('Cloning exporters repo...')
-            if (!await ssh.exists('/opt/dopamine/exporters/.git')) {
-                let shell = await program.shell()
-                await shell.exec('rm -rf exporters')
-                await shell.exec('git clone git@gitlab.dopamine.bg:devops/monitoring/exporters.git')
-                await shell.exec(`rsync -azpv exporters root@${hostIP}:/opt/dopamine`)
-                await shell.exec('rm -rf exporters')
-            }
-            await ssh.exec(`ln -sfv /opt/dopamine/exporters/mysqld_exporter ${optMysqlNodeExporter}`)
-            await ssh.exec(`chmod +x ${optMysqlNodeExporter}/mysqld_exporter`)
-
-            // Delete old service file
-            let oldOptMysqlExporter = '/etc/systemd/system/multi-user.target.wants/prometheus-mysqld-exporter.service';
-            if (await ssh.exists(oldOptMysqlExporter)) {
-                await ssh.exec('systemctl stop prometheus-mysqld-exporter.service')
-                await ssh.exec(`rm ${oldOptMysqlExporter}`)
-            }
-
-            // systemd service
-            await program.chat.notify('Creating mysql_exporter service...')
-
-            // set custom service file for devQA MySQL, because there're too many databases, that lead to memory leak
-            let mysqlExporterService = mysqlExporterServiceTemplate;
-            if (hostIP === devQAMySQLIP) {
-                mysqlExporterService = mysqlExporterService.replace(/{{ExecStart}}/, devQACustomExecStart)
-            } else {
-                mysqlExporterService = mysqlExporterService.replace(/{{ExecStart}}/, defaultExecStart)
-            }
-            await ssh.exec(`echo '${mysqlExporterService}' > /etc/systemd/system/mysqld_exporter.service`)
-
-            // Service start
-            await program.chat.notify('Starting service...')
-            await ssh.exec('systemctl daemon-reload')
-            await ssh.exec('systemctl enable mysqld_exporter.service')
-            await ssh.exec('systemctl restart mysqld_exporter.service')
-            await ssh.exec('systemctl status mysqld_exporter.service')
-
-            let isInstalled = await ssh.exec(`dpkg -s iptables-persistent > /dev/null 2>&1 && echo '1' || echo '0'`)
-
-            // Restore previous rules, prevent duplication
-            if (isInstalled === '0') {
-                await ssh.exec(`echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections`)
-                await ssh.exec(`echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections`)
-                await ssh.exec(`apt-get -y install iptables-persistent > /dev/null`)
-            }
-
-            await ssh.exec(`iptables-save > /etc/iptables/rules.v4`)
-            await ssh.exec(`cat /etc/iptables/rules.v4 | grep -v ${PORT} > /tmp/rules.v4 && mv /tmp/rules.v4 /etc/iptables/rules.v4`)
-            await ssh.exec('iptables-restore /etc/iptables/rules.v4')
-
-            // Setting security rules
-            await program.chat.notify('Setting security rules..')
-            await ssh.exec(`iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport ${PORT} -j DROP`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP1} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP2} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP3} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP4} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP5} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP6} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP7} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP8} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables-save > /etc/iptables/rules.v4`)
-
-        }
-        await program.sleep(2, 'Waiting a bit just in case');
-
-        // Check exporter works
-        await program.chat.notify('Checking exporter work... Please wait, it could take some time')
-        await ssh.exec(`netstat -plant | grep mysqld_export | grep -i listen || echo 'mysqld_exporter not running!!!'`)
-
-        // Check version
-        await ssh.exec(`/opt/mysqld_exporter/mysqld_exporter --version || echo 'mysqld_exporter not installed!!!'`)
-
-        await program.chat.notify('Success')
+    let checkNetToolsInstalled = await ssh.exec(`dpkg -l | grep nettools > /dev/null 2>&1 && echo '1' || echo '0'`)
+    if (!checkNetToolsInstalled) {
+        // Install net-tools
+        await ssh.exec('apt-get install -y net-tools > /dev/null')
     }
+
+    // Skip host if mysql is not installed
+    let mysqlPortInUse = await ssh.exec(`netstat -tulpn | grep LISTEN | grep ${MYSQL_PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
+    if (mysqlPortInUse === '0') return;
+
+    let portIsInUse = await ssh.exec(`netstat -tulpn | grep LISTEN | grep ${PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
+    let iptablesRulesExist = await ssh.exec(`iptables -L | awk -F ':' ' {print $2}' | sort -u | grep ${PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
+
+    if (!force && (portIsInUse === '1' && iptablesRulesExist === '1')) {
+        await program.chat.notify('Skipping host')
+    }
+    else {
+        // Install
+        await ssh.exec('rm -rfv /opt/mysqld_exporter') //temp
+        let optMysqlNodeExporter = '/opt/mysqld_exporter'
+        await program.chat.notify('Cloning exporters repo...')
+        if (!await ssh.exists('/opt/dopamine/exporters/.git')) {
+            let shell = await program.shell()
+            await shell.exec('rm -rf exporters')
+            await shell.exec('git clone git@gitlab.dopamine.bg:devops/monitoring/exporters.git')
+            await shell.exec(`rsync -azpv exporters root@${hostIP}:/opt/dopamine`)
+            await shell.exec('rm -rf exporters')
+        }
+        await ssh.exec(`ln -sfv /opt/dopamine/exporters/mysqld_exporter ${optMysqlNodeExporter}`)
+        await ssh.exec(`chmod +x ${optMysqlNodeExporter}/mysqld_exporter`)
+
+        // Delete old service file
+        let oldOptMysqlExporter = '/etc/systemd/system/multi-user.target.wants/prometheus-mysqld-exporter.service';
+        if (await ssh.exists(oldOptMysqlExporter)) {
+            await ssh.exec('systemctl stop prometheus-mysqld-exporter.service')
+            await ssh.exec(`rm ${oldOptMysqlExporter}`)
+        }
+
+        // systemd service
+        await program.chat.notify('Creating mysql_exporter service...')
+
+        // set custom service file for devQA MySQL, because there're too many databases, that lead to memory leak
+        let mysqlExporterService = mysqlExporterServiceTemplate;
+        if (hostIP === devQAMySQLIP) {
+            mysqlExporterService = mysqlExporterService.replace(/{{ExecStart}}/, devQACustomExecStart)
+        } else {
+            mysqlExporterService = mysqlExporterService.replace(/{{ExecStart}}/, defaultExecStart)
+        }
+        await ssh.exec(`echo '${mysqlExporterService}' > /etc/systemd/system/mysqld_exporter.service`)
+
+        // Service start
+        await program.chat.notify('Starting service...')
+        await ssh.exec('systemctl daemon-reload')
+        await ssh.exec('systemctl enable mysqld_exporter.service')
+        await ssh.exec('systemctl restart mysqld_exporter.service')
+        await ssh.exec('systemctl status mysqld_exporter.service')
+
+        let isInstalled = await ssh.exec(`dpkg -s iptables-persistent > /dev/null 2>&1 && echo '1' || echo '0'`)
+
+        // Restore previous rules, prevent duplication
+        if (isInstalled === '0') {
+            await ssh.exec(`echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections`)
+            await ssh.exec(`echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections`)
+            await ssh.exec(`apt-get -y install iptables-persistent > /dev/null`)
+        }
+
+        await ssh.exec(`iptables-save > /etc/iptables/rules.v4`)
+        await ssh.exec(`cat /etc/iptables/rules.v4 | grep -v ${PORT} > /tmp/rules.v4 && mv /tmp/rules.v4 /etc/iptables/rules.v4`)
+        await ssh.exec('iptables-restore /etc/iptables/rules.v4')
+
+        // Setting security rules
+        await program.chat.notify('Setting security rules..')
+        await ssh.exec(`iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport ${PORT} -j DROP`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP1} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP2} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP3} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP4} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP5} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP6} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP7} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP8} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables-save > /etc/iptables/rules.v4`)
+    }
+    await program.sleep(2, 'Waiting a bit just in case');
+
+    // Check exporter works
+    await program.chat.notify('Checking exporter work... Please wait, it could take some time')
+    await ssh.exec(`netstat -plant | grep mysqld_export | grep -i listen || echo 'mysqld_exporter not running!!!'`)
+
+    // Check version
+    await ssh.exec(`/opt/mysqld_exporter/mysqld_exporter --version || echo 'mysqld_exporter not installed!!!'`)
+
+    await program.chat.notify('Success')
 })
 
--- servers/monitoring/init-nodejs-exporter.js
+++ servers/monitoring/init-nodejs-exporter.js
@@ -31,13 +31,16 @@ const IP7 = '192.168.100.66';
 const IP8 = '192.168.110.66';
 
 program
-.description('Setup monitoring nodejs exporter')
+.description('Setup monitoring: Node Exporter')
 .option('-h, --hosts <list|all>', 'The target host names', { choices: Object.keys(cfg.hosts), required: true })
-.option('-n, --networks <list|all>', 'Networks', {required: true})
+.option('-n, --networks <list|all>', 'Networks', { choices: [... new Set(Object.values(cfg.hosts).map(i => i.network))], required: true})
 .option('-f, --force', 'Skip manual changes validations and proceed on your risk')
+.parse()
 
+// filter by network
+program.params.hosts = Object.values(cfg.hosts).filter(h => program.params.networks.includes(h.network)).map(i => i.name).join(',')
 
-.iterate('hosts', async (host) => {
+program.iterate('hosts', async (host) => {
     const params = program.params
     const force  = params.force !== undefined;
     let networks  = params.networks !== undefined ? params.networks : null;
@@ -45,101 +48,99 @@ program
 
     let hostNetwork = cfg.getHost(host).network;
 
-    // TODO: This will print that's executing on host, but it's NOT. Can it be fixed in someway
     // Execute only if the host network is included
-    if (networks.includes(hostNetwork)) {
-        let hostIP = cfg.getHost(host).ip;
+    if (!networks.includes(hostNetwork)) return;
 
-        console.log(`Starting script on HOST:(${host} : ${hostIP})...`)
-        await program.chat.notify(`Starting script on HOST:(${host} : ${hostIP})...`)
+    let hostIP = cfg.getHost(host).ip;
 
-        let ssh = await program.ssh(cfg.getHost(host).ip, 'root')
+    console.log(`Starting script on HOST:(${host} : ${hostIP})...`)
+    await program.chat.notify(`Starting script on HOST:(${host} : ${hostIP})...`)
 
-        let checkNetToolsInstalled = await ssh.exec(`dpkg -l | grep nettools > /dev/null 2>&1 && echo '1' || echo '0'`)
-        if (!checkNetToolsInstalled) {
-            // Install net-tools
-            await ssh.exec('apt-get install -y net-tools > /dev/null')
-        }
+    let ssh = await program.ssh(cfg.getHost(host).ip, 'root')
 
-        let portIsInUse = await ssh.exec(`netstat -tulpn | grep LISTEN | grep ${PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
-        let iptablesRulesExist = await ssh.exec(`iptables -L | awk -F ':' ' {print $2}' | sort -u | grep ${PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
-
-        if (!force && (portIsInUse === '1' && iptablesRulesExist === '1')) {
-            await program.chat.notify('Skipping host')
-        }
-        else {
-            // Install
-            await ssh.exec('rm -rfv /opt/node_exporter') //temp
-            let optNodeExporter = '/opt/node_exporter'
-            await program.chat.notify('Cloning exporters repo...')
-            if (!await ssh.exists('/opt/dopamine/exporters/.git')) {
-                let shell = await program.shell()
-                await shell.exec('rm -rf exporters')
-                await shell.exec('git clone git@gitlab.dopamine.bg:devops/monitoring/exporters.git')
-                await shell.exec(`rsync -azpv exporters root@${hostIP}:/opt/dopamine`)
-                await shell.exec('rm -rf exporters')
-            }
-            await ssh.exec(`ln -sfv /opt/dopamine/exporters/node_exporter ${optNodeExporter}`)
-            await ssh.exec(`chmod +x ${optNodeExporter}/node_exporter`)
-
-            // Create user
-            await program.chat.notify('Creating node_exporter user...')
-            await ssh.exec('if ! grep -q node_exporter /etc/passwd ; then useradd -rs /bin/false node_exporter; fi')
-
-            // Delete old service file
-            let nodeExporterService = '/etc/systemd/system/multi-user.target.wants/node_exporter.service';
-            if (await ssh.exists(nodeExporterService)) {
-                await ssh.exec(`rm ${nodeExporterService}`)
-            }
-
-            // systemd service
-            await program.chat.notify('Creating node_exporter service...')
-            await ssh.exec(`echo '${nodejsExporterService}' > /etc/systemd/system/node_exporter.service`)
-
-            // Service start
-            await program.chat.notify('Starting service...')
-            await ssh.exec('systemctl daemon-reload')
-            await ssh.exec('systemctl enable node_exporter.service')
-            await ssh.exec('systemctl restart node_exporter.service')
-            await ssh.exec('systemctl status node_exporter.service')
-
-            let isInstalled = await ssh.exec(`dpkg -s iptables-persistent > /dev/null 2>&1 && echo '1' || echo '0'`)
-
-            // Restore previous rules, prevent duplication
-            if (isInstalled === '0') {
-                await ssh.exec(`echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections`)
-                await ssh.exec(`echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections`)
-                await ssh.exec(`apt-get -y install iptables-persistent > /dev/null`)
-            }
-
-            await ssh.exec(`iptables-save > /etc/iptables/rules.v4`)
-            await ssh.exec(`cat /etc/iptables/rules.v4 | grep -v ${PORT} > /tmp/rules.v4 && mv /tmp/rules.v4 /etc/iptables/rules.v4`)
-            await ssh.exec('iptables-restore /etc/iptables/rules.v4')
-
-            // Setting security rules
-            await program.chat.notify('Setting security rules..')
-            await ssh.exec(`iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport ${PORT} -j DROP`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP1} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP2} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP3} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP4} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP5} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP6} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP7} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables -I INPUT -p tcp -s ${IP8} --dport ${PORT} -j ACCEPT`)
-            await ssh.exec(`iptables-save > /etc/iptables/rules.v4`)
-
-        }
-        await program.sleep(2, 'Waiting a bit just in case');
-
-        // Check exporter works
-        await program.chat.notify('Checking exporter work... Please wait, it could take some time')
-        await ssh.exec(`netstat -plant | grep node_exporter | grep -i listen || echo 'node_exporter not running'`)
-
-        // Check version
-        await ssh.exec(`/opt/node_exporter/node_exporter --version || echo 'node_exporter not installed!!!'`)
-
-        await program.chat.notify('Success')
+    let checkNetToolsInstalled = await ssh.exec(`dpkg -l | grep nettools > /dev/null 2>&1 && echo '1' || echo '0'`)
+    if (!checkNetToolsInstalled) {
+        // Install net-tools
+        await ssh.exec('apt-get install -y net-tools > /dev/null')
     }
+
+    let portIsInUse = await ssh.exec(`netstat -tulpn | grep LISTEN | grep ${PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
+    let iptablesRulesExist = await ssh.exec(`iptables -L | awk -F ':' ' {print $2}' | sort -u | grep ${PORT} > /dev/null 2>&1 && echo '1' || echo '0'`)
+
+    if (!force && (portIsInUse === '1' && iptablesRulesExist === '1')) {
+        await program.chat.notify('Skipping host')
+    }
+    else {
+        // Install
+        await ssh.exec('rm -rfv /opt/node_exporter') //temp
+        let optNodeExporter = '/opt/node_exporter'
+        await program.chat.notify('Cloning exporters repo...')
+        if (!await ssh.exists('/opt/dopamine/exporters/.git')) {
+            let shell = await program.shell()
+            await shell.exec('rm -rf exporters')
+            await shell.exec('git clone git@gitlab.dopamine.bg:devops/monitoring/exporters.git')
+            await shell.exec(`rsync -azpv exporters root@${hostIP}:/opt/dopamine`)
+            await shell.exec('rm -rf exporters')
+        }
+        await ssh.exec(`ln -sfv /opt/dopamine/exporters/node_exporter ${optNodeExporter}`)
+        await ssh.exec(`chmod +x ${optNodeExporter}/node_exporter`)
+
+        // Create user
+        await program.chat.notify('Creating node_exporter user...')
+        await ssh.exec('if ! grep -q node_exporter /etc/passwd ; then useradd -rs /bin/false node_exporter; fi')
+
+        // Delete old service file
+        let nodeExporterService = '/etc/systemd/system/multi-user.target.wants/node_exporter.service';
+        if (await ssh.exists(nodeExporterService)) {
+            await ssh.exec(`rm ${nodeExporterService}`)
+        }
+
+        // systemd service
+        await program.chat.notify('Creating node_exporter service...')
+        await ssh.exec(`echo '${nodejsExporterService}' > /etc/systemd/system/node_exporter.service`)
+
+        // Service start
+        await program.chat.notify('Starting service...')
+        await ssh.exec('systemctl daemon-reload')
+        await ssh.exec('systemctl enable node_exporter.service')
+        await ssh.exec('systemctl restart node_exporter.service')
+        await ssh.exec('systemctl status node_exporter.service')
+
+        let isInstalled = await ssh.exec(`dpkg -s iptables-persistent > /dev/null 2>&1 && echo '1' || echo '0'`)
+
+        // Restore previous rules, prevent duplication
+        if (isInstalled === '0') {
+            await ssh.exec(`echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections`)
+            await ssh.exec(`echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections`)
+            await ssh.exec(`apt-get -y install iptables-persistent > /dev/null`)
+        }
+
+        await ssh.exec(`iptables-save > /etc/iptables/rules.v4`)
+        await ssh.exec(`cat /etc/iptables/rules.v4 | grep -v ${PORT} > /tmp/rules.v4 && mv /tmp/rules.v4 /etc/iptables/rules.v4`)
+        await ssh.exec('iptables-restore /etc/iptables/rules.v4')
+
+        // Setting security rules
+        await program.chat.notify('Setting security rules..')
+        await ssh.exec(`iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport ${PORT} -j DROP`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP1} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP2} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP3} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP4} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP5} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP6} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP7} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables -I INPUT -p tcp -s ${IP8} --dport ${PORT} -j ACCEPT`)
+        await ssh.exec(`iptables-save > /etc/iptables/rules.v4`)
+    }
+    await program.sleep(2, 'Waiting a bit just in case');
+
+    // Check exporter works
+    await program.chat.notify('Checking exporter work... Please wait, it could take some time')
+    await ssh.exec(`netstat -plant | grep node_exporter | grep -i listen || echo 'node_exporter not running'`)
+
+    // Check version
+    await ssh.exec(`/opt/node_exporter/node_exporter --version || echo 'node_exporter not installed!!!'`)
+
+    await program.chat.notify('Success')
 })
 
