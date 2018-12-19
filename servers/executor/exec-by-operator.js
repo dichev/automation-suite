85dc9bfc35bc3bf4af838bd77f2b2a5de82dfa02
dichev <g.dichev@gmail.com>
2018-12-19 11:30:20 +0200

    Exec scripts: support repeating mode

--- servers/executor/exec-by-operator.js
+++ servers/executor/exec-by-operator.js
@@ -18,36 +18,48 @@ program
     .option('--no-history', 'Disable saving commands to history (useful for credentials data)')
     .parse()
 
-if (program.params.exec && program.params.execFile) {
-    console.warn(`! Please set only one of these: --exec or --exec-file`)
-    process.exit(1)
+
+const iterate = async (cmd) => {
+    await program.iterate('operators', async (operator) => {
+        let ssh = await new SSHClient().connect({host: cfg.getLocationByOperator(operator).hosts.web1, username: 'dopamine'})
+        await ssh.chdir('/home/dopamine/production/' + cfg.operators[operator].dir)
+        await ssh.exec(cmd)
+        await ssh.disconnect()
+    })
 }
 
 
 Promise.resolve().then(async () => {
+    if (program.params.exec && program.params.execFile) {
+        console.warn(`! Please set only one of these: --exec or --exec-file`)
+        process.exit(1)
+    }
     program.params.force = true
     console.log('Executing over:\n ' + program.params.operators.split(',').join('\n '))
     
     let cmd = ''
     if(program.params.exec) {
         cmd = program.params.exec
+        await iterate(cmd)
     } else if(program.params.execFile){
         cmd = fs.readFileSync(program.params.execFile, 'utf8').replace(/\r?\n/g, '\n')
+        await iterate(cmd)
     } else {
         console.log('Enter command:')
         let input = new Input({collectHistoryFile: program.params.history === false ? '' : __dirname + '/.history'})
-        cmd = await input.ask('>') || 'echo Hello $HOSTNAME!'
+        while (true) {
+            cmd = (await input.ask('>')).trim()
+            if (!cmd) continue;
+            if (cmd === 'exit') break;
+            await iterate(cmd)
+            console.log('')
+        }
     }
-
-    program.iterate('operators', async (operator) => {
-        let ssh = await new SSHClient().connect({host: cfg.getLocationByOperator(operator).hosts.web1, username: 'dopamine'})
-        await ssh.chdir('/home/dopamine/production/' + cfg.operators[operator].dir)
-        await ssh.exec(cmd)
-        await ssh.disconnect()
-    })
   
 })
 
-    
+
+
+
 
 
--- servers/executor/exec.js
+++ servers/executor/exec.js
@@ -19,35 +19,42 @@ program
     .option('--no-history', 'Disable saving commands to history (useful for credentials data)')
     .parse()
 
-if (program.params.exec && program.params.execFile) {
-    console.warn(`! Please set only one of these: --exec or --exec-file`)
-    process.exit(1)
+
+const iterate = async (cmd) => {
+    await program.iterate('hosts', async (host) => {
+        let ssh = await new SSHClient().connect({host: cfg.getHost(host).ip, username: program.params.user})
+        await ssh.exec(cmd)
+        await ssh.disconnect()
+    })
 }
 
+
 Promise.resolve().then(async () => {
+    if (program.params.exec && program.params.execFile) {
+        console.warn(`! Please set only one of these: --exec or --exec-file`)
+        process.exit(1)
+    }
     program.params.force = true
     console.log('Executing over:\n ' + program.params.hosts.split(',').join('\n '))
     
     let cmd = ''
     if(program.params.exec) {
         cmd = program.params.exec
+        await iterate(cmd)
     } else if(program.params.execFile){
         cmd = fs.readFileSync(program.params.execFile, 'utf8').replace(/\r?\n/g, '\n')
+        await iterate(cmd)
     } else {
         console.log('Enter command:')
         let input = new Input({collectHistoryFile: program.params.history === false ? '' : __dirname + '/.history'})
-        cmd = await input.ask('>') || 'echo Hello $HOSTNAME!'
+        while (true) {
+            cmd = (await input.ask('>')).trim()
+            if (!cmd) continue;
+            if (cmd === 'exit') break;
+            await iterate(cmd)
+            console.log('')
+        }
     }
-
-    program.iterate('hosts', async (host) => {
-        // console.log(`root@${host}:~# ls ${cmd}`)
-        let ssh = await new SSHClient().connect({host: cfg.getHost(host).ip, username: program.params.user})
-        await ssh.exec(cmd)
-        await ssh.disconnect()
-        
-        // repeat?
-        // if(answer === 'exit') process.exit()
-    })
   
 })
 
