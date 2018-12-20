48bf5802fa8f4a0156a8aa59302ee9c0161245b4
plamen <p.yonchev@dopamine.bg>
2018-12-20 16:04:50 +0200

    remove host from command execution(it will be unified everywhere)

--- servers/backup/run.js
+++ servers/backup/run.js
@@ -13,23 +13,12 @@ program
 .option('-t, --type <list|all>', 'The target host names', { choices: ['full', 'incr'], required: true })
 
 .iterate('hosts', async (host) => {
-    const params = program.params
-    const backupType  = params.type ;
+    const backupType  = program.params.type ;
 
     let hostIP = cfg.getHost(host).ip;
     console.log(`Starting script on HOST:(${host} : ${hostIP})...`)
-    await program.chat.notify(`Starting script on HOST:(${host} : ${hostIP})...`)
-
-    console.log(host)
-    let ssh = await program.ssh(cfg.getHost(host).ip, 'root')
-
-    let mysqlHost = await ssh.exec(`cat /root/.my.cnf | grep host | cut -d'=' -f 2`) //@ по добре .my.cnf да се унифицира, вместо да се workaround-ва
-    let mysqlHostParam = '';
-    if (mysqlHost !== '') {
-        mysqlHostParam = ` -H ${mysqlHost}`;
-    }
-    //@ съответно може директно mysqlHost = '127.0.0.1'
 
     await program.chat.notify(`Starting full backup`)
-    await ssh.execBackground(`/opt/pyxbackup/pyxbackup ${backupType} ${mysqlHostParam}`)
+    let ssh = await program.ssh(hostIP, 'root')
+    await ssh.execBackground(`/opt/pyxbackup/pyxbackup ${backupType}`)
 })
