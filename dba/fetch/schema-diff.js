cd9f86c0d69c972fedc585d1805a32efd88eb959
dichev <g.dichev@gmail.com>
2018-12-21 19:20:26 +0200

    Port schema-diff script

---
+++ dba/fetch/schema-diff.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+'use strict';
+
+const Program = require('dopamine-toolbox').Program
+const Shell = require('dopamine-toolbox').Shell
+const SSHClient = require('dopamine-toolbox').SSHClient
+const MySQL = require('dopamine-toolbox').MySQL
+const dump = require('dopamine-toolbox').MySQLDumper.dump
+
+const diff = require('diff')
+const fs = require('fs')
+const path = require('path')
+const cfg = require('configurator')
+const colors = require('colors/safe')
+
+const DEST = path.resolve(__dirname + '/../../.tmp/schema-diff').replace(/\\/g, '/')
+const DATABASES = ['platform', 'demo', 'panel', 'jackpot', 'stats', 'segments', 'tournaments', 'rewards', 'bonus', 'archive']
+
+
+let program = new Program()
+program
+    .description('Compare database schemas of the operators')
+    .option('-o, --operators <name>', 'The target operator name', { required: true, choices: Object.keys(cfg.operators) })
+    .option('--base <operator>', 'The operator which database will be used as base for the comparison. By default will be used the first one from the --operators list')
+    .parse()
+
+
+if(program.params.base && !program.params.operators.split(',').includes(program.params.base)) {
+    throw Error(`The base operator (${program.params.base}) must be include in the operators list (${program.params.operators})`)
+}
+let baseOperator = program.params.base || program.params.operators.split(',')[0]
+
+
+program.run(async () => {
+    if (!fs.existsSync(DEST)) {
+        console.log(`creating folder ${DEST}`)
+        await new Shell().exec(`mkdir -pv ${DEST}`)
+    }
+})
+
+// Dump database schemas
+.then(async () => program.iterate('operators', async (operator) => {
+    let dbs = cfg.databases[cfg.operators[operator].databases]
+    let ronly = cfg.access.mysql.readOnly
+
+    let sshMaster = await new SSHClient().connect({host: dbs.backups.master, username: 'dopamine'})
+    let dbMaster = await new MySQL().connect({user: ronly.user, password: ronly.password}, sshMaster)
+    
+    let sshArchive = await new SSHClient().connect({host: dbs.backups.archive, username: 'dopamine'})
+    let dbArchive = await new MySQL().connect({user: ronly.user, password: ronly.password}, sshArchive)
+    
+    
+    for (let dbType of DATABASES) {
+        let db = dbType === 'archive' ? dbArchive : dbMaster
+        let dbname = cfg.getOperatorDatabaseName(operator, dbType)
+        console.warn(`\nDumping ${dbname}..`)
+        await db.query(`USE ${dbname};`)
+        
+        await dump({
+            connection: db.getConnection(),
+            
+            excludeTables: [
+                '__sync_users_bet_limits_default_prev',
+                // 'games_configuration_view',
+                'bonus_usage_report_archive', // TMP due permission error
+                'view_rtp_monthly' // TMP due permission error
+            ],
+            dest: DEST + '/' + dbname + '.sql',
+            modifiers: [
+                (output) => output.replace(/ AUTO_INCREMENT=\d+/g, ''),
+                (output) => output.replace(/ DEFINER=`.+`/g, ''),
+                (output) => output.replace(/ SQL SECURITY INVOKER/g, ''),
+                (output) => output.replace(/\(PARTITION [\s\S]+ ENGINE = InnoDB\) /g, ' --- list of available partitions ---\n'),
+            ],
+            sortKeys: true,
+            exportData: false // be careful with this option on mirrors
+        })
+        
+    }
+    
+    await Promise.all([dbMaster, dbArchive, sshMaster, sshArchive].map(async conn => await conn.disconnect()))
+}))
+
+// Disable parallelism here to ensure readability fo the diffs report bellow
+.then(() => {
+    program.params.parallel = undefined
+})
+
+
+// Diff all database schema files
+.then(async() => program.iterate('operators', async (operator) => {
+    if(operator === baseOperator) return // not necessary to compare him against himself
+    
+    console.log(`\n\n################### DIFF ${operator} vs ${baseOperator} ###################`)
+    
+    for (let dbType of DATABASES) {
+        const BASE = cfg.getOperatorDatabaseName(baseOperator, dbType) + '.sql'
+        const TARGET = cfg.getOperatorDatabaseName(operator, dbType) + '.sql'
+        
+        let base   = fs.readFileSync(DEST + '/' + BASE, 'utf8');
+        let target = fs.readFileSync(DEST + '/' + TARGET, 'utf8');
+        
+        let diffs = diff.diffLines(target, base);
+        
+        let totalDiffs = 0
+        console.log(`# Compare ${TARGET} against ${BASE}..`)
+        diffs.forEach((part, i) => {
+            // green for additions, red for deletions, grey for common parts
+            let color = part.added ? 'green' : part.removed ? 'red' : 'grey';
+            if (part.added || part.removed) {
+                totalDiffs += part.count
+                if (part.added) {
+                    console.info(colors.green(part.value.trim().replace(/^/mg, '+')));
+                } else {
+                    console.info(colors.red(part.value.trim().replace(/^/mg, '-')));
+                }
+            }
+        })
+        if (totalDiffs > 0) {
+            console.info(`Total diffs: ${totalDiffs} lines`)
+            console.info('-'.repeat(60))
+        }
+    }
+}))
+    
