90a350ccd850a81feb77fe6b38d5a889f2ebd7f7
n.terziev <n.terziev@dopamine.bg>
2019-02-05 18:38:56 +0200

    Add new file whitelist-check

---
+++ servers/nginx/whitelist-check.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+'use strict';
+
+const Program = require('dopamine-toolbox').Program
+const cfg = require('configurator')
+const Shell = require('dopamine-toolbox').Shell
+const fs = require('fs')
+const RootDir = require('os').tmpdir().replace(/\\/g, '/') + '/dope-'+ Date.now() // windows compatibility
+
+let program = new Program({chat: ''})
+let shell = new Shell()
+fs.mkdirSync(RootDir)
+String.prototype.grep = function(reg){ reg = new RegExp(reg,'i'); return this.split("\n").filter( line => line.match(reg) ).join("\n") }
+
+program
+    .description("List whitelisted ips for operator")
+    .option('-o, --operators <list|all>', `Comma-separated list of operators`, {choices: Object.keys(cfg.operators), required: true})
+    .iterate('operators', async (operator) => {
+        let location = cfg.getLocationByOperator(operator).name
+        let gitProject = 'servers-conf-' + location
+        let configFile = `${RootDir}/${gitProject}/nginx/conf.d/allow/${operator}.conf`
+
+        await shell.chdir(RootDir)
+        try{
+            await shell.exec(`git clone git@gitlab.dopamine.bg:servers/servers-conf-${location}.git`,{silent:true})
+        }catch(e){
+            await shell.chdir(`${RootDir}/${gitProject}`)
+            await shell.exec('git pull',{silent:true})
+        }
+        if( fs.existsSync(configFile)) console.log(fs.readFileSync(configFile).toString().grep('^allow'))
+    })
