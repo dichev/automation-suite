d82f2f5ef3ac3a3c8a892024fc4cb968e90ea7ce
dichev <g.dichev@gmail.com>
2019-02-13 18:50:27 +0200

    Switch to the right gcloud project & clean up

--- servers/gcloud/resize-vm-web.js
+++ servers/gcloud/resize-vm-web.js
@@ -5,41 +5,44 @@ const Program = require('dopamine-toolbox').Program
 const Shell = require('dopamine-toolbox').Shell
 const SSHClient = require('dopamine-toolbox').SSHClient
 const cfg = require('configurator')
+
+const ONLY_GCLOUD_WEB_HOSTS = Object.values(cfg.hosts).filter(h => h.type === 'web' && cfg.locations[h.location].gcloud).map(h => h.name)
+
 let program = new Program()
-
-
 program
     .description('Resize webs VM on Google Cloud without downtime')
-    .option('-h, --hosts <list|all>', 'The target host names', { choices: Object.values(cfg.hosts).filter(h => h.type === 'web'), required: true })
+    .option('-h, --hosts <list|all>', 'The target host names', { choices: ONLY_GCLOUD_WEB_HOSTS, required: true })
     
 
-program.iterate('hosts', async (name) => { // TODO: check is the web on google cloud
+program.iterate('hosts', async (name) => {
     let shell = new Shell()
     let host = cfg.hosts[name]
-    let webId = cfg.locations[host.location].hosts.webs.find(w => w.name === name)
-    let web = await new SSHClient().connect({host: cfg.getHost(host).ip, username: 'root'})
+    const WEB = cfg.locations[host.location].hosts.webs.find(w => w.name === name)
     
-    // 0. switch user and projects
-    await program.chat.message(`Disable traffic to ${webId}`)
-    await shell.exec(`node servers/nginx/switch-webs-by-location -l ${host.location} -w all --exclude-webs ${webId} --no-chat`)
+    // 0. switch webs traffic
+    await program.chat.message(`Disable traffic to ${WEB}`)
+    await shell.exec(`node servers/nginx/switch-webs-by-location -l ${host.location} -w all --exclude-webs ${WEB} --no-chat`)
     
     
     // 1. stop crontab on web1
-    if(webId === 'web1'){
+    if(WEB === 'web1'){
         await program.chat.message(`Stopping cron jobs..`)
-        await web.exec('systemctl stop crontab') // it will be auto started after the vm restart
+        let ssh = await new SSHClient().connect({host: host.ip, username: 'root'})
+        await ssh.exec('systemctl stop crontab') // it will be auto started after the vm restart
+        await ssh.disconnect()
         await program.sleep(60, 'Waiting cronjobs to finish just in case')
     }
     
     // 2. gcloud compute instances stop NAME
     await program.confirm('Stop the VM?')
     await program.chat.message(`Stopping VM..`)
+    await shell.exec(`gcloud config set project ${cfg.locations[host.name].gcloud.project}`)
     await shell.exec(`gcloud compute instances stop ${host.name}`) // TODO: possible name conflicts with google cloud instance name
     
     
     // 3. gcloud compute instances set-machine-type NAME --machine-type custom-4-1024 4=4vCPU, 1024=1G RAM
     await program.chat.message(`Changing VM resources..`)
-    await shell.exec(`gcloud compute set-machine-type ${host.name}
+    await shell.exec(`gcloud compute set-machine-type ${host.name} \
         --custom-cpu=${host.resources.cpu} \
         --custom-memory=${host.resources.memory}
     `)
@@ -57,9 +60,9 @@ program.iterate('hosts', async (name) => { // TODO: check is the web on google c
     await shell.exec(`node servers/php-binary/check -h ${host.name} --no-chat`)
     
     // 6. enable operators
-    await program.chat.message(`Activate traffic to ${webId}`)
+    await program.chat.message(`Activate traffic to ${WEB}`)
     await shell.exec(`node servers/nginx/switch-webs-by-location -l ${host.location} -w all --no-chat`)
     
     
-    await web.disconnect()
+    
 })
