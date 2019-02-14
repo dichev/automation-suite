86dbd98c33784fd767f4326d8465944c98b2746a
n.terziev <n.terziev@dopamine.bg>
2019-02-14 14:36:33 +0200

    Update setup.js

--- servers/docker/setup.js
+++ servers/docker/setup.js
@@ -34,7 +34,6 @@ program
             apt-get install -q -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
             curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - 
-            apt-key fingerprint 0EBFCD88
             add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
             apt-get -qq update
