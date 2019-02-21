73e589572c3d6a2f37a8101f999dd86328404496
dichev <g.dichev@gmail.com>
2019-02-21 15:21:53 +0200

    Configurator: Support exclusion

--- configurator/operators.js
+++ configurator/operators.js
@@ -10,14 +10,23 @@ String.prototype.grep = function(reg){ reg = new RegExp(reg,'i'); return this.sp
 program
     .description('Show operators by location')
     .option('-l, --locations <list|all>', `Comma-separated list of Locations`, {choices: Object.keys(cfg.locations), required: true})
-    .option('-g, --grep <string>', `Expression to apply as filter`, {required: false})
-    .iterate('locations', async (location) => {
-        let operators = Object.values(cfg.operators).filter(o => o.location === location ).map(o => o.name)
-        if(program.params.grep){
-           operators = operators
+    .option('--exclude', 'All operators from the list will be prefixes for exclusion')
+    .option('-g, --grep <string>', `Expression to apply as filter`)
+    
+    .run(async () => {
+        const LOCATIONS = program.params.locations.split(',')
+        let operators = Object.values(cfg.operators).filter(o => LOCATIONS.includes(o.location)).map(o => o.name)
+        
+        if(program.params.exclude){
+            operators = operators.map(o => `-${o}`)
+        }
+        
+        if (program.params.grep) {
+            operators = operators
                 .join('\n')
                 .grep(program.params.grep)
                 .split('\n')
         }
+        
         console.log(operators.join(','))
     })
