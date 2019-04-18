f6796c92d23b1e159b4e7065e44b2d4e5f652daa
d.dimov <d.dimov@dopamine.bg>
2019-04-18 16:41:19 +0300

    Rename activate-jackpot to jackpot, allow single platform/jackpot migrations (or both) execute DB migration while spins are locked

--- deploy/hermes/jackpot.js
+++ deploy/hermes/jackpot.js
@@ -15,45 +15,61 @@ const REPO = "d:/www/hermes/master"
 let program = new Program({ chat: cfg.chat.rooms.deployBackend })
 
 program
-    .description(`Activate jackpot on operator`)
+    .description(`Execute jackpot related migrations with spin locking`)
     .option('-o, --operator <string>', 'The target operator name', {required: true, choices: Object.keys(cfg.operators)})
     .option('--jackpotSeed <string>', 'The target jackpot seed (must be located in seed/next folder)', {required: false})
-    .option('--platformSeed <string>', 'The target platform seed (must be located in seed/envs/next folder)', {required: true})
+    .option('--platformSeed <string>', 'The target platform seed (must be located in seed/envs/next folder)', {required: false})
     .example(`
-        node deploy/hermes/activate-jackpot.js -o adjarabet --platformSeed adjarabet-next
-        node deploy/hermes/activate-jackpot.js -o mansion --jackpotSeed mansionPop --platformSeed mansion-jackpot
+        node deploy/hermes/jackpot.js -o mansion --jackpotSeed mansionPop
+        node deploy/hermes/jackpot.js -o mansion --platformSeed mansion-jackpot
+        node deploy/hermes/jackpot.js -o mansion --jackpotSeed mansionPop --platformSeed mansion-jackpot
     `)
     .parse()
 
 
 program.run(async () => {
+    if (!program.params.platformSeed && !program.params.jackpotSeed) throw Error('You must provide at least one migration.')
+    
+    const readFile = function(fileName) {
+        if (!fs.existsSync(fileName)) throw Error(`${fileName} does not exist.`)
+        
+        return fs.readFileSync(fileName).toString()
+    }
+    
+    const areThereSpinsInProgress = async function(db) {
+        let res = await db.query('SELECT count(id) as spins FROM transactions_round_instance WHERE statusCode = 100')
+        
+        return parseInt(res[0].spins || 0)
+    }
+    
     const operator = program.params.operator
+    const DB_NAME_PLATFORM = `${cfg.operators[operator].dbPrefix}platform`
+    const DB_NAME_JACKPOT  = `${cfg.operators[operator].dbPrefix}jackpot`
     
     console.log(`Ensure the repos are up to date`)
     let shell = new Shell()
     await shell.exec(`cd ${REPO}/platform && git fetch --quiet --tags && git reset --hard origin/master`)
     await shell.exec(`cd ${REPO}/jackpot && git fetch --quiet --tags && git reset --hard origin/master`)
     
-    const PLATFORM_SEED_FILE = `${REPO}/platform/.migrator/seed/envs/next/${program.params.platformSeed}.sql`
-    if (!fs.existsSync(PLATFORM_SEED_FILE)) {
-        throw Error(`${PLATFORM_SEED_FILE} does not exist.`)
+    let platformMigration  = null
+    let jackpotMigration   = null
+    
+    let PLATFORM_SEED_FILE = null
+    let JACKPOT_SEED_FILE  = null
+    
+    
+    if (program.params.platformSeed) {
+        PLATFORM_SEED_FILE = `${REPO}/platform/.migrator/seed/envs/next/${program.params.platformSeed}.sql`
+        platformMigration  = readFile(PLATFORM_SEED_FILE)
     }
-    const platformMigration = fs.readFileSync(PLATFORM_SEED_FILE).toString()
     
-    
-    let jackpotMigration = null
-    let JACKPOT_SEED_FILE = null
     if (program.params.jackpotSeed) {
-        JACKPOT_SEED_FILE  = `${REPO}/jackpot/.migrator/seed/next/${program.params.jackpotSeed}.sql`
-        if (!fs.existsSync(JACKPOT_SEED_FILE)) {
-            throw Error(`${JACKPOT_SEED_FILE} does not exist.`)
-        }
-    
         if (cfg.operators[operator].sharedJackpot) {
             throw Error(`Shared db ${cfg.operators[operator].sharedJackpot} is not yet supported.`)
         }
-        
-        jackpotMigration  = fs.readFileSync(JACKPOT_SEED_FILE).toString()
+    
+        JACKPOT_SEED_FILE = `${REPO}/jackpot/.migrator/seed/next/${program.params.jackpotSeed}.sql`
+        jackpotMigration  = readFile(JACKPOT_SEED_FILE)
     }
     
     let dbs = cfg.databases[cfg.operators[operator].databases]
@@ -70,25 +86,15 @@ program.run(async () => {
     let db2 = new MySQL()
     await db.connect({user: 'root'}, ssh)
     await db2.connect({user: 'root'}, ssh)
-
-    const dbNamePlatform = `${cfg.operators[operator].dbPrefix}platform`
     
-    if (jackpotMigration) {
-        const dbNameJackpot = `${cfg.operators[operator].dbPrefix}jackpot`
+    await program.confirm('Do you want to begin?')
     
-        await program.chat.message(`• Executing ${JACKPOT_SEED_FILE}`)
-        await db.query(`USE ${dbNameJackpot};`)
-        await db.query(jackpotMigration)
-    
-        await program.chat.message('_Please validate jackpot config is OK_')
-    }
-
-    await program.confirm('Continue with platform migration?')
-    await db.query(`USE ${dbNamePlatform}`)
-    await db2.query(`USE ${dbNamePlatform}`)
+    await db.query(`USE ${DB_NAME_PLATFORM}`)
     
     await program.chat.message('• Locking spins')
     await db.query('LOCK TABLE `currencies_exchange_rates` WRITE;')
+    
+    await db2.query(`USE ${DB_NAME_PLATFORM}`)
     
     let spinsInProgress = await areThereSpinsInProgress(db2)
     let retries = 0
@@ -102,8 +108,19 @@ program.run(async () => {
         if (++retries > 30) throw new Error(`Failed to lock spins (there were rounds in progress for more than ${retries} sec)`)
     }
     
-    await program.chat.message(`• Executing ${PLATFORM_SEED_FILE}`)
-    await db2.query(platformMigration)
+    if (jackpotMigration) {
+        await db2.query(`USE ${DB_NAME_JACKPOT}`)
+    
+        await program.chat.message(`• Executing ${JACKPOT_SEED_FILE}`)
+        await db2.query(jackpotMigration)
+    }
+    
+    if (platformMigration) {
+        await db2.query(`USE ${DB_NAME_PLATFORM}`)
+    
+        await program.chat.message(`• Executing ${PLATFORM_SEED_FILE}`)
+        await db2.query(platformMigration)
+    }
     
     await program.chat.message('• Unlocking spins')
     await db.query('UNLOCK TABLE;')
@@ -114,9 +131,3 @@ program.run(async () => {
     await db2.disconnect()
     await ssh.disconnect()
 })
-
-async function areThereSpinsInProgress(db) {
-    let res = await db.query('SELECT count(id) as spins FROM transactions_round_instance WHERE statusCode = 100')
-    
-    return parseInt(res[0].spins || 0)
-}
