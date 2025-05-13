462739845889c1ddcfe281ff9881138bb3420d92
dichev <g.dichev@gmail.com>
2025-05-13 13:35:47 -0700

    Whitelist ips for operator

---
+++ servers/nginx/whitelist.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+'use strict';
+
+const Program = require('dopamine-toolbox').Program
+const cfg = require('configurator')
+const Shell = require('dopamine-toolbox').Shell
+const Netmask = require('netmask').Netmask                  // Toolbox ?
+const util = require('util')                                // Toolbox ?
+const fs = require('fs')
+const uuid = require('uuid/v1')
+const whois = util.promisify(require('whois').lookup)       // можеби е по-добре да са в туулбокса
+const dnsReverse = util.promisify(require('dns').reverse)   // и да се връшат promisify- нати
+const RootDir = require('os').tmpdir() + '/dope-'+ uuid()
+const Econf = {silent:true}
+const Url = "https://jira.dopamine.bg/browse"
+const now = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+
+//let program = new Program({chat: cfg.chat.rooms.deployBackend})
+let program = new Program({chat: ''})
+let shell = new Shell()
+fs.mkdirSync(RootDir)
+
+// TODO! Maybe add them to toolbox
+String.prototype.grep = function(reg){ reg = new RegExp(reg,'i'); return this.split("\n").filter( line => line.match(reg) ).join("\n") }
+Array.prototype.unique = function(){ return this.filter( (value,index) => this.indexOf(value) === index  ) }
+Array.prototype.duplicates = function(){ return this.filter( value => this.indexOf(value) !== this.lastIndexOf(value)  ).unique() }
+
+program
+    .description("Whitelist ips for operator")
+    .option('-o, --operators <list|all>', `Comma-separated list of operators`, {choices: Object.keys(cfg.operators), required: true})
+    .option('-i, --ips <list|all>',`Comma-separated list of ips to be checked/added`, {required:true})
+    .option('-t, --task <string>',`Task to be added to allow list`, {required:true})
+    .iterate('operators', async (operator) => {
+        let me =  await shell.exec('git config user.name',Econf)
+        let ips = program.params.ips.split(',')
+        let task = program.params.task
+        let location = cfg.getLocationByOperator(operator).name
+        let gitProject = 'servers-conf-' + location
+        let branch = me + '/' +task
+        let configFile = `${RootDir}/${gitProject}/nginx/conf.d/allow/${operator}.conf`
+        let existingIps = []
+
+        if(ips.length !== ips.unique().length)
+            throw `You provided duplicate ips. Please check at ${Url}/${task}\n` + ips.duplicates().join(', ')
+
+        await shell.chdir(RootDir)
+        await shell.exec(`git clone git@gitlab.dopamine.bg:servers/servers-conf-${location}.git`,Econf)
+
+        let currentConfig = fs.readFileSync(configFile).toString()
+
+        for (let ip of ips){
+                console.log("=============[ " + ip + " ]===============")
+                if(currentConfig.grep(ip).length){
+                    console.log('Address exists in current list.')
+                    existingIps.push(ip)
+                    continue
+                }
+                let mask = new Netmask(ip)
+                let whoisData = (await whois(ip)).grep('^Country:|^City:|^Netname:|^Address:|^OrgName:')
+                let reverseDns = 'Not found'
+                try{ reverseDns = await dnsReverse(ip) } catch(e){}
+                console.log([
+                    whoisData,
+                    '----------------',
+                    `mask: [${mask.size}] ${mask.first} - ${mask.last}, reverse-dns: ${reverseDns}`,
+                    '----------------',
+                ].join("\n"))
+        }
+        console.log("Currently listed")
+        console.log(currentConfig.grep('^allow'))
+        await program.confirm(`Approved or not?`)
+        await shell.chdir(RootDir + '/' + gitProject)
+        let currentMaster = await shell.exec('git rev-parse --short master',Econf)
+        await shell.exec(`git checkout -b ${branch}`,Econf)
+
+        fs.appendFileSync(configFile,'\n# ' + now.padEnd(26,' ') + "#" + task)
+        for (let ip of ips) {
+            if(existingIps.indexOf(ip) === -1 ) fs.appendFileSync(configFile,'\nallow\t' + (ip + ';').padEnd(20,' ') + '#' + task)
+        }
+
+        await shell.exec(`git commit ${configFile} -m "Relative to ${Url}/${task}"`,Econf)
+        console.log(fs.readFileSync(configFile).toString().grep('^allow'))
+        await shell.exec(`git push --set-upstream origin ${branch}`)
+        console.log('#Deploy changes using those commands:\n'
+            + ` servers/servers-conf/list-changes -l ${location}\n`
+            + ` servers/servers-conf/update -l ${location} --reload nginx\n`
+            + `### servers/servers-conf/update -l ${location} --rev ${currentMaster} --reload nginx\n`
+            + ` deploy/hermes-env/check  -p 10 -o ${operator}\n`
+        )
+    })
+// !TODO remove RootDir
